8dc36cf17c56e63854562fe816e46e49492eeb8a
Neikan <sladkov.e.m@outlook.com>
2020-05-13 15:03:20 +0300

    Add template ServiceWorker

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,6 @@
 import FilmData from "../models/film";
 import CommentData from "../models/comment";
-
-const RequestStatusCode = {
-  OK: 200,
-  MULTIPLE: 300,
-  BAD_REQUEST: 400,
-  NOT_FOUND: 404,
-  SERVER_ERROR: 500
-};
+import {RequestStatusCode} from "../consts";
 
 
 const Method = {
--- src/consts.js
+++ src/consts.js
@@ -1,3 +1,12 @@
+export const RequestStatusCode = {
+  OK: 200,
+  MULTIPLE: 300,
+  BAD_REQUEST: 400,
+  NOT_FOUND: 404,
+  SERVER_ERROR: 500
+};
+
+
 export const KeyCode = {
   ENTER: 13,
   ESC: 27
